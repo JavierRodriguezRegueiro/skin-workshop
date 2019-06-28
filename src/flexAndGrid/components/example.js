a69018071c5e410fe7c4018f8623c6426351eb6f
javierrodriguezregueiro <javiro238@gmail.com>
2019-06-28 11:59:32 +0200

    Change label in example

--- src/flexAndGrid/components/example.js
+++ src/flexAndGrid/components/example.js
@@ -10,7 +10,7 @@ class Example extends React.Component {
                 <textarea className='example__textarea example__textarea--html' defaultValue={this.props.html}></textarea>
                 <textarea className='example__textarea example__textarea--style' defaultValue={this.props.style}></textarea>
                 <iframe title={this.props.title} className='example__code' id='example'></iframe>
-                <button className='example__btn'>Try it</button>
+                <button className='example__btn'>Try it!</button>
             </section>
         );
     }
