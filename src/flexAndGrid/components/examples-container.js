47756c82e88db5ba6c9ae969faf1285bc0fc7ba4
javierrodriguezregueiro <javiro238@gmail.com>
2019-07-01 22:57:27 +0200

    Added guide buttons in flex and grid examples

--- src/flexAndGrid/components/examples-container.js
+++ src/flexAndGrid/components/examples-container.js
@@ -1,57 +1,61 @@
 import React from 'react';
 import Example from './example';
 import '../styles/examples-container.css'
+import FloatingButton from "./floating-button";
 
 
-const topics = ['Flexbox', 'CSS Grid'];
+const topics = ['Flexbox', 'CSS Grid'],
 
-const flexExamples = [
-    {
-        id: 'ex1-flex',
-        title: 'Example 1',
-        desc: 'description for example 1, follow this using flexbox.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    },
-    {
-        id: 'ex2-flex',
-        title: 'Example 2',
-        desc: 'description for example 2, follow this using flexbox.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    },
-    {
-        id: 'ex3-flex',
-        title: 'Example 3',
-        desc: 'description for example 3, follow this using flexbox.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    }
-];
 
-const gridExamples = [
-    {
-        id: 'ex1-grid',
-        title: 'Example 1',
-        desc: 'description for example 1, follow this using grid.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    },
-    {
-        id: 'ex2-grid',
-        title: 'Example 1',
-        desc: 'description for example 2, follow this using grid.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    },
-    {
-        id: 'ex3-grid',
-        title: 'Example 1',
-        desc: 'description for example 3, follow this using grid.',
-        html: '<div></div>',
-        style: '.gg {color:red}'
-    }
-]
+    flexExamples = [
+        {
+            id: 'ex1-flex',
+            title: 'Example 1',
+            desc: 'description for example 1, follow this using flexbox.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        },
+        {
+            id: 'ex2-flex',
+            title: 'Example 2',
+            desc: 'description for example 2, follow this using flexbox.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        },
+        {
+            id: 'ex3-flex',
+            title: 'Example 3',
+            desc: 'description for example 3, follow this using flexbox.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        }
+    ],
+
+    gridExamples = [
+        {
+            id: 'ex1-grid',
+            title: 'Example 1',
+            desc: 'description for example 1, follow this using grid.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        },
+        {
+            id: 'ex2-grid',
+            title: 'Example 1',
+            desc: 'description for example 2, follow this using grid.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        },
+        {
+            id: 'ex3-grid',
+            title: 'Example 1',
+            desc: 'description for example 3, follow this using grid.',
+            html: '<div></div>',
+            style: '.gg {color:red}'
+        }
+    ],
+    gridGuide = 'https://css-tricks.com/snippets/css/complete-guide-grid/',
+    flexGuide= 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/';
 
 class ExamplesContainer extends React.Component {
     constructor(props) {
@@ -72,28 +76,32 @@ class ExamplesContainer extends React.Component {
     }
 
     whichExamples() {
-        let examples = 'none';
+        let examples = 'none',
+            questionLink;
+
         if (this.state.mainTopic === 'Flexbox') {
             examples = flexExamples;
+            questionLink = flexGuide;
         } else if (this.state.mainTopic === 'CSS Grid') {
             examples = gridExamples;
+            questionLink = gridGuide;
         }
-        return examples;
+        return {examples, questionLink};
     }
 
     renderExamples() {
-        let examplesToRender = this.whichExamples();
-        if (examplesToRender === 'none') {
+        let operators = this.whichExamples();
+        if (operators.examples === 'none') {
             return <ChooseTopic/>
         } else {
-            return <ExampleContainerBody examples={examplesToRender}/>
+            return <ExampleContainerBody examples={operators.examples} questionLink={operators.questionLink}/>
         }
     }
 
     render() {
         return (
             <article className='examplesContainer'>
-                <TopicSelector topics={topics} setTopic={this.setTopic} currentTopic={this.state.mainTopic} />
+                <TopicSelector topics={topics} setTopic={this.setTopic} currentTopic={this.state.mainTopic}/>
                 {this.renderExamples()}
             </article>
         );
@@ -115,7 +123,7 @@ class TopicSelector extends React.Component {
         return (
             <div className={this.selectorClass()}>
                 {this.props.topics.map(topic => <button className='topic-selector__selector' key={topic}
-                                                   onClick={() => this.props.setTopic(topic)}>{topic}</button>)}
+                                                        onClick={() => this.props.setTopic(topic)}>{topic}</button>)}
             </div>
         );
     }
@@ -134,12 +142,13 @@ const ExampleContainerBody = (props) => {
                     style={ex.style}
                 />
             })}
+            <FloatingButton questionLink={props.questionLink} label='Guide'/>
         </section>
     );
 }
 
 const ChooseTopic = () => {
-    return(
+    return (
         <section className='ChooseTopic'>
             <h3 className='ChooseTopic__title'>Let's start!</h3>
             <h4 className='ChooseTopic__subtitle'>Please, choose one topic to start/continue with this workshop</h4>
---
+++ src/flexAndGrid/components/floating-button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/floating-button.css';
+const FloatingButton = (props) => {
+    return(
+        <button className='floating-button' onClick={() => window.open(props.questionLink, '_blank')}>{props.label}</button>
+    );
+}
+
+export default FloatingButton;
